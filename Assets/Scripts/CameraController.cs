eb966e6f81cc9ddd7082f77e19b38cfe2bf316c6
jinuj <jinu.jacob90@gmail.com>
2016-09-06 14:17:47 -0400

    removed duplication of player and camera upon changing levels

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,9 +7,19 @@ public class CameraController : MonoBehaviour {
 	private Vector3 targetPos;
 	public float moveSpeed;
 
+	private static bool cameraExists;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		
+		// prevent duplication of camera
+		if (!cameraExists) {
+			cameraExists = true;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,28 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
-	private Animator anim;
-	private bool playerMoving;
-	private Vector2 lastMove;
+	public Vector2 lastMove;
 
+	private Animator anim;
+	private Rigidbody2D playerRigidBody;
+	private bool playerMoving;
+
+	private static bool playerExists;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		playerRigidBody = GetComponent<Rigidbody2D> ();
+
+		// prevent duplication of player
+		if (!playerExists) {
+			playerExists = true;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+
+
 	}
 	
 	// Update is called once per frame
@@ -21,15 +35,25 @@ public class PlayerController : MonoBehaviour {
 
 		// Update character position
 		if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) > 0.5f) {
-			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			//transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidBody.velocity.y);
 			playerMoving = true;
 			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 		}
 
 		if (Mathf.Abs(Input.GetAxisRaw ("Vertical")) > 0.5f) {
-			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			//transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
 			playerMoving = true;
 			lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.05f) {
+			playerRigidBody.velocity = new Vector2 (0f, playerRigidBody.velocity.y);
+		}
+
+		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.05f) {
+			playerRigidBody.velocity = new Vector2 (playerRigidBody.velocity.x, 0f);
 		}
 
 		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
