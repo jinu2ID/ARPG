cbe12bca82ff234e47cb8094a92a542c533e034e
jinuj <jinu.jacob90@gmail.com>
2016-09-05 22:25:47 -0400

    added script to load new scenes

---
+++ Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LoadNewArea : MonoBehaviour {
+	public string levelToLoad;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.name == "Player") {
+			SceneManager.LoadScene (levelToLoad);
+		}
+	}
+
+}
