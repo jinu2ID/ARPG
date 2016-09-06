3b1f258726ee16821cdf5d576ee7cc7fc6dc5d3c
jinuj <jinu.jacob90@gmail.com>
2016-09-05 22:24:47 -0400

    added house to test map and created house interior map

--- Assets/Maps/base tileset.tsx
+++ Assets/Maps/base tileset.tsx
@@ -69,4 +69,62 @@
    <object id="1" x="0" y="0.125" width="15.625" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="1217">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="15.75">
+    <polygon points="0,0 16,0 15.875,-15.875 15.375,-15.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1218">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0">
+    <polygon points="0,0 0,16 16.125,16 16.375,15.125 0.875,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1271">
+  <objectgroup draworder="index">
+   <object id="3" x="1" y="0.25" width="13.25" height="15.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="1273">
+  <objectgroup draworder="index">
+   <object id="3" x="0.625" y="1.125" width="12.375" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="1274">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0" width="16" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="1275">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.125" width="15.875" height="15.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1328">
+  <objectgroup draworder="index">
+   <object id="3" x="1" y="0.125" width="14.75" height="13.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="1330">
+  <objectgroup draworder="index">
+   <object id="2" x="0.25" y="0.25" width="14.625" height="13.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1331">
+  <objectgroup draworder="index">
+   <object id="7" x="0.125" y="0.125">
+    <polygon points="0,0 15.875,0 16,3 2.875,16 0,16.125 0,15.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1332">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0.125">
+    <polygon points="0,0 16,-0.125 15.75,15.75 13.125,15.875 -0.125,2.625 0,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
