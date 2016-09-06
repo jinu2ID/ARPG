d1f3bd5979431bb9f778e64fc278358724c7ddc4
jinuj <jinu.jacob90@gmail.com>
2016-09-06 14:19:34 -0400

    added script for start points

---
+++ Assets/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStartPoint : MonoBehaviour {
+
+	private PlayerController player;
+	private CameraController camera;
+
+	public Vector2 startDirection;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+		camera = FindObjectOfType<CameraController> ();
+
+		player.transform.position = transform.position;
+		player.lastMove = startDirection;
+		camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
